073c3670bff0e2da7f14a5dda0e0ed17fa3fc785
Tomek <klingaroo@gmail.com>
2022-04-25 14:04:21 -0400

    adds local simplyE db connection testing

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,8 @@
 from datetime import datetime
+import os
 
 import pytest
+import yaml
 
 from overdrive_reconcile import marc_file
 
@@ -17,3 +19,29 @@ def mock_dst_fh(monkeypatch, test_dst):
         return test_dst
 
     monkeypatch.setattr(marc_file, "create_dst_fh", _patch)
+
+
+def get_creds(library: str):
+    fh = None
+    if library == "BPL":
+        fh = ".simplyE/bpl_simply_e.yaml"
+    elif library == "NYPL":
+        fh = ".simplyE/nyp_simply_e.yaml"
+
+    with open(os.path.join(os.environ["USERPROFILE"], fh), "r") as f:
+        data = yaml.safe_load(f)
+        return data
+
+
+@pytest.fixture
+def local_bpl_connection():
+    creds = get_creds("BPL")
+    conn = f"postgresql://{creds.get('USER')}:{creds.get('PASSWORD')}@{creds.get('HOST')}/{creds.get('DATABASE')}"
+    return conn
+
+
+@pytest.fixture
+def local_nypl_connection():
+    creds = get_creds("NYPL")
+    conn = f"postgresql://{creds.get('USER')}:{creds.get('PASSWORD')}@{creds.get('HOST')}/{creds.get('DATABASE')}"
+    return conn
---
+++ tests/test_connection.py
@@ -0,0 +1,46 @@
+import os
+
+import pytest
+import yaml
+from sqlalchemy import create_engine, text
+from sqlalchemy.engine.row import LegacyRow
+
+
+@pytest.mark.local
+def test_bpl_conn(local_bpl_connection):
+    engine = create_engine(local_bpl_connection)
+    with engine.connect() as conn:
+        results = conn.execute(
+            text(
+                """
+                SELECT i.identifier FROM identifiers i 
+                JOIN licensepools lp ON i.id=lp.identifier_id 
+                WHERE lp.licenses_owned > 0 AND i.type='Overdrive ID'
+                LIMIT 2
+                """
+            )
+        )
+        for r in results:
+            assert isinstance(r, LegacyRow)
+            assert isinstance(r[0], str)
+            assert len(r[0]) == 36
+
+
+@pytest.mark.local
+def test_nypl_conn(local_nypl_connection):
+    engine = create_engine(local_nypl_connection)
+    with engine.connect() as conn:
+        results = conn.execute(
+            text(
+                """
+                SELECT i.identifier FROM identifiers i 
+                JOIN licensepools lp ON i.id=lp.identifier_id 
+                WHERE lp.licenses_owned > 0 AND i.type='Overdrive ID'
+                LIMIT 2
+                """
+            )
+        )
+        for r in results:
+            assert isinstance(r, LegacyRow)
+            assert isinstance(r[0], str)
+            assert len(r[0]) == 36
