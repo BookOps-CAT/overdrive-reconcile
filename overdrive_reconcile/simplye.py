268abd8bfcfdd9870ad38866e29ba4edb1772bf7
Tomek <klingaroo@gmail.com>
2022-04-27 14:45:21 -0400

    adds SimplyE methods

---
+++ overdrive_reconcile/simplye.py
@@ -0,0 +1,57 @@
+"""
+Methods to interact with SimplyE databases
+"""
+import os
+
+import pandas as pd
+from sqlalchemy import create_engine
+from sqlalchemy.engine.base import Engine
+import yaml
+
+
+RESERVE_ID_QUERY = """
+    SELECT i.identifier FROM identifiers i 
+    JOIN licensepools lp ON i.id=lp.identifier_id 
+    WHERE lp.licenses_owned > 0 AND i.type='Overdrive ID'
+    LIMIT 5
+"""
+
+
+def get_cred_fh(library: str) -> str:
+    """
+    Determines correct SimplyE credential file
+    """
+    if library == "BPL":
+        return ".simplyE/bpl_simply_e.yaml"
+    elif library == "NYPL":
+        return ".simplyE/nyp_simply_e.yaml"
+    else:
+        raise ValueError("Invalid library code passsed")
+
+
+def get_simplye_creds(library: str) -> dict:
+    """
+    Retrieves SimplyE database credentials for 'NYPL' or 'BPL' library
+
+    Args:
+        library:                'NYPL' or 'BPL'
+    """
+    fh = get_cred_fh(library)
+
+    with open(os.path.join(os.environ["USERPROFILE"], fh), "r") as f:
+        data = yaml.safe_load(f)
+        return data
+
+
+def simplye_connection(library: str) -> Engine:
+    """
+    Creates sqlalchemy connectable object (engine) to be used
+    to query SimplyE database
+
+    Args:
+        library:                'NYPL' or 'BPL'
+    """
+    creds = get_simplye_creds(library)
+    conn = f"postgresql://{creds.get('USER')}:{creds.get('PASSWORD')}@{creds.get('HOST')}/{creds.get('DATABASE')}"
+    engine = create_engine(conn)
+    return engine
---
+++ tests/test_simplye.py
@@ -0,0 +1,38 @@
+import pytest
+
+from sqlalchemy.engine.base import Engine
+
+from overdrive_reconcile.simplye import (
+    get_cred_fh,
+    get_simplye_creds,
+    simplye_connection,
+)
+
+
+@pytest.mark.parametrize(
+    "arg, expectation",
+    [("NYPL", ".simplyE/nyp_simply_e.yaml"), ("BPL", ".simplyE/bpl_simply_e.yaml")],
+)
+def test_get_cred_fh(arg, expectation):
+    assert get_cred_fh(arg) == expectation
+
+
+def test_get_cred_fh_exception():
+    with pytest.raises(ValueError):
+        get_cred_fh(None)
+
+
+@pytest.mark.local
+@pytest.mark.parametrize("arg", ["BPL", "NYPL"])
+def test_simplye_creds(arg):
+    result = get_simplye_creds(arg)
+
+    assert isinstance(result, dict)
+    assert result is not {}
+    assert sorted(result.keys()) == ["DATABASE", "HOST", "PASSWORD", "USER"]
+
+
+@pytest.mark.local
+def test_simplye_connection():
+    result = simplye_connection("NYPL")
+    assert isinstance(result, Engine)
