435f3d9a21e0c5db369386179fc01a9fe9b431a5
Tomek <klingaroo@gmail.com>
2022-08-05 09:45:41 -0400

    adds basic local scraping test

--- tests/test_webscraper.py
+++ tests/test_webscraper.py
@@ -1,8 +1,17 @@
+from datetime import datetime
+
 import pytest
 
 from overdrive_reconcile.webscraper import scrape
 
 
+@pytest.mark.local
 def test_scrape(mock_main_dir, test_main_dir):
     src = "tests/for-deletion-sample.csv"
-    scrape("BPL", src)
+    scrape("BPL", src, 2)
+
+    today = datetime.now().date()
+    with open(
+        f"{test_main_dir}/{today}/BPL-FINAL-for-deletion-verified-resources.csv", "r"
+    ) as f:
+        assert len(f.read().strip()) > 0
