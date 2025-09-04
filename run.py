27739b658e941e3febbac0b581d843b6fb1e60b9
Charlotte Kostelic <charlottekostelic@gmail.com>
2025-09-04 16:31:57 -0400

    removed unused `check-overdrive` command from run.py

--- run.py
+++ run.py
@@ -5,18 +5,18 @@ import sys
 
 from overdrive_reconcile.reconcile import reconcile
 from overdrive_reconcile.utils import count_rows, date_subdirectory, logger_dict_config
-from overdrive_reconcile.webscraper import check_status, scrape
+from overdrive_reconcile.webscraper import scrape
 
 
 def main(args: list) -> None:
     # process: str: library: str, src_fh: str)
     config = logger_dict_config()
     logging.config.dictConfig(config)
-    logger = logging.getLogger("overdrive_reconcile")
 
     parser = argparse.ArgumentParser(
         prog="Overdrive-Reconcile",
-        description="Sets of scripts reconciling records between Sierra and OverDrive platform.",
+        description="Sets of scripts reconciling records between Sierra "
+        "and OverDrive platform.",
     )
 
     parser.add_argument(
@@ -24,10 +24,9 @@ def main(args: list) -> None:
         help=(
             "'reconcile' runs entire set of scripts | "
             "'webscrape' runs only webscraping of OverDrive catalog | "
-            "'check-overdrive' runs scraping for a single resource"
         ),
         type=str,
-        choices=["reconcile", "webscrape", "check-overdrive"],
+        choices=["reconcile", "webscrape"],
     )
     parser.add_argument(
         "library", help="'BPL' or 'NYPL'", type=str, choices=["BPL", "NYPL"]
@@ -62,10 +61,6 @@ def main(args: list) -> None:
         else:
             total = count_rows(pargs.source) - 1
             scrape(pargs.library, pargs.source, total, pargs.start)
-
-    if pargs.action == "check-overdrive":
-        logger.info(f"Checking status of {pargs.source} on OverDrive platform.")
-        check_status(pargs.source, pargs.library)
 
 
 if __name__ == "__main__":
