458f74640b4947de44a1bd49de003229b12f02be
Charlotte Kostelic <charlottekostelic@gmail.com>
2025-09-08 14:34:22 -0400

    updated get_overdrive_api_creds

--- overdrive_reconcile/overdrive_session.py
+++ overdrive_reconcile/overdrive_session.py
@@ -1,12 +1,14 @@
 """Functions that allow for interactions with Overdrive Discovery APIs"""
 
+import datetime
 import os
+from typing import Generator
 
 import yaml
 from bookops_overdrive import OverdriveAccessToken, OverdriveSession
 
 
-def get_overdrive_api_creds(library: str) -> dict[str, str]:
+def get_overdrive_api_creds(library: str) -> None:
     """
     Retrieves Overdrive API credentials for 'NYPL' or 'BPL' library
     Args:
@@ -14,15 +16,25 @@ def get_overdrive_api_creds(library: str) -> dict[str, str]:
     Returns:
         API credentials for the given library as a dictionary
     """
-    if library.upper() == "NYPL":
+    library = library.upper()
+    if library == "NYPL":
         fh = ".cred/.overdrive/nyp_overdrive_api.yaml"
-    elif library.upper() == "BPL":
+        os.environ["OVERDRIVE_URL"] = "http://ebooks.nypl.org/ContentDetails.htm?ID="
+    elif library == "BPL":
+        os.environ["OVERDRIVE_URL"] = (
+            "http://digitalbooks.brooklynpubliclibrary.org/ContentDetails.htm?ID="
+        )
         return NotImplemented
     else:
         raise ValueError("Invalid library code passed")
     with open(os.path.join(os.environ["USERPROFILE"], fh), "r") as f:
         data = yaml.safe_load(f)
-        return data
+    if expired_coll_token(data["COLL_TOKEN_EXPIRATION"]):
+        data = refresh_collection_token(
+            creds=data, cred_file=os.path.join(os.environ["USERPROFILE"], fh)
+        )
+    for k, v in data.items():
+        os.environ[f"{library}_{k}"] = v
 
 
 def get_inventory(library: str) -> list[str]:
@@ -44,3 +56,27 @@ def get_inventory(library: str) -> list[str]:
         inventory = session.get(response.json()["files"][0]["fileUrl"])
         out.extend(inventory.json()["reserveIds"])
     return out
+
+
+def refresh_collection_token(creds: dict[str, str], cred_file: str) -> dict[str, str]:
+    token = OverdriveAccessToken(key=creds["CLIENT_KEY"], secret=creds["CLIENT_SECRET"])
+    with OverdriveSession(authorization=token) as session:
+        response = session.get_library_account_info(int(creds["LIBRARY_ID"]))
+    creds["COLL_TOKEN"] = response.json()["collectionToken"]
+    new_expiration = datetime.datetime.now() + datetime.timedelta(days=30)
+    creds["COLL_TOKEN_EXPIRATION"] = new_expiration.strftime("%Y-%m-%d")
+    with open(cred_file, "w") as fh:
+        yaml.dump(creds, fh, default_flow_style=False)
+    return creds
+
+
+def expired_coll_token(token_expire: str) -> bool:
+    today = datetime.datetime.now()
+    if datetime.datetime.strptime(token_expire, "%Y-%m-%d") >= today:
+        return False
+    return True
+
+
+def get_batches(iterable: list[str], size: int) -> Generator[list[str], None, None]:
+    for i in range(0, len(iterable), size):
+        yield iterable[i : i + size]
--- tests/test_overdrive_session.py
+++ tests/test_overdrive_session.py
@@ -7,24 +7,21 @@ from overdrive_reconcile.overdrive_session import (
 )
 
 
-@pytest.mark.local
-def test_get_overdrive_api_creds_nypl():
+def test_get_overdrive_api_creds_nypl(mock_creds):
     get_overdrive_api_creds("nypl")
 
     assert "NYPL_CLIENT_KEY" in os.environ.keys()
     assert "NYPL_CLIENT_SECRET" in os.environ.keys()
-    assert "NYPL_ILS_NAME" in os.environ.keys()
     assert "NYPL_LIBRARY_ID" in os.environ.keys()
-    assert "NYPL_WEBSITE_ID" in os.environ.keys()
+    assert "NYPL_COLL_TOKEN" in os.environ.keys()
+    assert "NYPL_COLL_TOKEN_EXPIRATION" in os.environ.keys()
 
 
-@pytest.mark.local
 def test_get_overdrive_api_creds_bpl():
     result = get_overdrive_api_creds("bpl")
     assert result == NotImplemented
 
 
-@pytest.mark.local
 def test_get_overdrive_api_creds_other():
     with pytest.raises(ValueError) as exc:
         get_overdrive_api_creds("foo")
