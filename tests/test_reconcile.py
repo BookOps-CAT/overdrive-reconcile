99c80339e538945b66e389c7a0894702994229f8
Charlotte Kostelic <charlottekostelic@gmail.com>
2025-09-09 12:51:08 -0400

    removed duplicate test from `test_reconcile`

--- tests/test_reconcile.py
+++ tests/test_reconcile.py
@@ -1,16 +1,6 @@
 import os
 
-import pandas as pd
-
-from overdrive_reconcile.reconcile import dedup_on_reserve_id, reconcile
-from overdrive_reconcile.utils import date_subdirectory
-
-
-def test_dedup_on_reserve_id():
-    subdir = date_subdirectory("BPL")
-    d = {"bib_no": ["b1", "b2", "b2", "b3"], "reserve_id": [1, 2, 2, 1]}
-    df = pd.DataFrame(data=d)
-    dedup_on_reserve_id("BPL", df, subdir)
+from overdrive_reconcile.reconcile import reconcile
 
 
 def test_reconcile(
